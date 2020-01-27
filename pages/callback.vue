65542e49867556750458d7a0a1cc93d5c410251e
T.YAMASHITA <8256tako@gmail.com>
2020-01-27 15:37:35 +0900

    :sparkles: Sign in to Spotify

---
+++ pages/callback.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  middleware({ redirect, store, query }) {
+    store.dispatch('login/signIn', query.code)
+
+    redirect('/')
+  }
+}
+</script>
---
+++ store/login.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const state = () => ({
+  isSignedIn: false
+})
+
+export const mutations = {
+  setSignIn(state, isSignedIn) {
+    state.isSignedIn = isSignedIn
+  }
+}
+
+export const actions = {
+  async signIn({ commit, state }, code) {
+    await axios.post(`http://0.0.0.0:8000/login?code=${code}`).then((_) => {
+      commit('setSignIn', true)
+    })
+  }
+}
