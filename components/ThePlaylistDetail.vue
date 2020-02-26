98aa15baf2cbe7c4cf7c46f6dc31b15bf7cc40cd
T.YAMASHITA <8256tako@gmail.com>
2020-02-26 20:27:32 +0900

    :lipstick: Show playlist details

--- components/ThePlaylistDetail.vue
+++ components/ThePlaylistDetail.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="component-playlist-detail">
-    <img :src="imageUrl" />
+    <img class="component-playlist-detail__img" :src="imageUrl" />
+    <div class="component-playlist-detail__information">
+      <span class="component-playlist-detail__information__name">
+        {{ name }}
+      </span>
+      <span class="component-playlist-detail__information__owner">
+        Created by {{ owner }}
+      </span>
+      <span class="component-playlist-detail__information__description">
+        {{ description }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -9,20 +20,48 @@ export default {
   props: {
     description: {
       type: String,
-      required: true
+      required: true,
+      default: ''
     },
     imageUrl: {
       type: String,
-      required: true
+      required: true,
+      default: ''
     },
     name: {
       type: String,
-      required: true
+      required: true,
+      default: ''
     },
     owner: {
       type: String,
-      required: true
+      required: true,
+      default: ''
     }
   }
 }
 </script>
+
+<style lang="sass">
+.component-playlist-detail
+  display: flex
+  flex-direction: column
+  &__img
+    margin: auto
+    width: 50%
+  &__information
+    display: flex
+    flex-direction: column
+    &__name
+      padding-top: 16px
+      font-size: 2.0em
+      font-weight: 800
+      text-align: center
+    &__owner
+      padding: 8px 0
+      text-align: center
+    &__description
+      font-size: 1.2em
+      text-align: center
+      opacity: 0.5
+</style>
