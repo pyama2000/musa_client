a18e4ad42c30f7cf1cb4988689ad58ea4f516fb4
T.YAMASHITA <8256tako@gmail.com>
2020-06-25 17:22:20 +0900

    :lipstick: Remove vuetify from layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,5 @@
 <template>
-  <v-app dark>
-    <the-header />
-    <nuxt class="nuxt__pages" />
-  </v-app>
+  <div>
+    <nuxt />
+  </div>
 </template>
-
-<script>
-import TheHeader from '~/components/TheHeader'
-
-export default {
-  components: {
-    TheHeader
-  }
-}
-</script>
-
-<style lang="sass">
-.nuxt__pages
-  padding: 16px
-</style>
