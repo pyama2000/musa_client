48f8e8fe05c8e34df6ee1c4285d420f42efa98e6
T.YAMASHITA <8256tako@gmail.com>
2020-06-26 01:33:38 +0900

    :lipstick: Create Player.vue

--- components/neumorphism/Player.vue
+++ components/neumorphism/Player.vue
@@ -6,6 +6,17 @@
     <div class="player__track-name">
       {{ object.track.name }}
     </div>
+    <div class="player__buttons">
+      <div class="player__buttons__item">
+        <i class="material-icons">skip_previous</i>
+      </div>
+      <div class="player__buttons__item">
+        <i class="material-icons">pause</i>
+      </div>
+      <div class="player__buttons__item">
+        <i class="material-icons">skip_next</i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,13 +35,16 @@ export default {
 <style lang="scss" scoped>
 .player {
   display: grid;
-  height: 40%;
-  grid-template-columns: 40% 1fr;
+  grid-template-columns: 120px 1fr;
+  grid-template-rows: 1.5fr 0.5fr;
+  gap: 24px;
+  grid-template-areas: 'image track-name' 'buttons buttons';
   align-items: center;
   padding: 16px 8px;
   background: #eef0f4;
 
   &__image {
+    grid-area: image;
     display: flex;
     width: 120px;
     height: 120px;
@@ -42,6 +56,27 @@ export default {
       width: 80%;
       height: 80%;
       margin: auto;
+    }
+  }
+
+  &__track-name {
+    grid-area: track-name;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    font-weight: bold;
+  }
+
+  &__buttons {
+    grid-area: buttons;
+    display: flex;
+    justify-content: space-around;
+
+    &__item {
+      padding: 12px;
+      background: #eef0f4;
+      border-radius: 100%;
+      box-shadow: 9.91px 9.91px 15px #d9dade, -9.91px -9.91px 15px #ffffff;
     }
   }
 }
