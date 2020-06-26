6fe922ed2fb1ff63af0da89b70ad8eae93ad546c
T.YAMASHITA <8256tako@gmail.com>
2020-06-26 14:58:29 +0900

    :sparkles: Show playlists

---
+++ components/neumorphism/PlaylistCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="playlist-card">
+    <image-sheet
+      :src="playlist.image_url"
+      alt="Playlist image"
+      class="playlist-card__image"
+    />
+    <div class="playlist-card__title">
+      {{ playlist.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    ImageSheet: () => import('~/components/neumorphism/ImageSheet')
+  },
+  props: {
+    playlist: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.playlist-card {
+  display: grid;
+  grid-template-rows: 88px 1fr;
+  gap: 24px;
+  width: 120px;
+  height: 180px;
+  padding: 24px 16px;
+  background: #eef0f4;
+  border-radius: 15px;
+  box-shadow: 9.91px 9.91px 15px #d9dade, -9.91px -9.91px 15px #ffffff;
+
+  &__title {
+    width: 100%;
+    margin-top: auto;
+    font-size: xx-small;
+    font-weight: bold;
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
+</style>
--- pages/home.vue
+++ pages/home.vue
@@ -3,6 +3,12 @@
     <div class="home__card">
       <player :object="currentPlayingTrack" />
 
+      <div class="playlists__container">
+        <div v-for="(playlist, i) in playlists" :key="i">
+          <playlist-card :playlist="playlist" />
+        </div>
+      </div>
+
       <link-button-list class="link-button-list" />
     </div>
   </div>
@@ -19,17 +25,15 @@ export default {
   },
   components: {
     Player: () => import('~/components/neumorphism/Player'),
-    LinkButtonList: () => import('~/components/neumorphism/LinkButtonList')
+    LinkButtonList: () => import('~/components/neumorphism/LinkButtonList'),
+    PlaylistCard: () => import('~/components/neumorphism/PlaylistCard')
   },
   async asyncData({ $axios }) {
-    const { data, status } = await $axios.get('/player/current')
+    let currentPlayingTrack = null
+    await $axios.get('/player/current').then(({ data, status }) => {
+      if (status !== 200) return
 
-    if (status !== 200) {
-      return { currentPlayingTrack: null }
-    }
-
-    return {
-      currentPlayingTrack: {
+      currentPlayingTrack = {
         isPlaying: data.is_playing,
         track: {
           id: data.track.id,
@@ -37,6 +41,16 @@ export default {
           name: data.track.name
         }
       }
+    })
+
+    let playlists = null
+    await $axios.get('/playlists').then(({ data }) => {
+      playlists = data.followed_playlists
+    })
+
+    return {
+      currentPlayingTrack,
+      playlists
     }
   }
 }
@@ -44,12 +58,27 @@ export default {
 
 <style lang="scss" scoped>
 .home__card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
   padding-top: 16px;
   padding-bottom: 16px;
   background: #eef0f4;
 }
 
-.link-button-list {
-  margin-top: 24px;
+.playlists__container {
+  display: flex;
+  margin: 24px 16px;
+  padding: 24px;
+  background: #eef0f4;
+  border-radius: 15px;
+  box-shadow: 9.91px 9.91px 15px #d9dade, -9.91px -9.91px 15px #ffffff;
+  overflow: scroll;
+  -webkit-overflow-scrolling: touch;
+
+  & > * {
+    margin: 0 8px;
+  }
 }
 </style>
