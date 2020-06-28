088311b478dde5808f6a4ebb8187d43e9f93a31a
T.YAMASHITA <8256tako@gmail.com>
2020-06-29 00:15:42 +0900

    :lipstick: Add login layout css

--- layouts/login.vue
+++ layouts/login.vue
@@ -3,3 +3,14 @@
     <nuxt />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.login-layout {
+  height: -webkit-fill-available;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: $color;
+  background: $background;
+}
+</style>
